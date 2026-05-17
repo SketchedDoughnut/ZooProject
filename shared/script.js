dc055cd90bda0ff230a177a0d1baaa3b4dd9a740
Sketched Doughnut <noreply>
2026-05-17 00:24:27 -0700

    Another test to see if it'll work or if it'll break..

--- shared/script.js
+++ shared/script.js
@@ -12,9 +12,13 @@ const isElementLoaded = async selector => {
 };
 
 /**
- * Redirects to a different webpage, from root.
+ * Redirects to a different webpage.
  * @param {string} page 
+ * @param {boolean} add_url whether to add the current URL to the input or not.
  */
-function redirect(page) {
-  window.location.href = page
+function redirect(page, add_url = false) {
+  let newUrl = ''
+  if (add_url) { newUrl += window.location.toString }
+  // window.location.href = newUrl += page
+  window.location.assign(newUrl + page)
 }
