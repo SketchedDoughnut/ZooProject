5ced6fd69bc8ee66e009f29466b8ad2dbde67d86
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2026-05-21 17:55:35 -0700

    Update game.js

--- game/js/game.js
+++ game/js/game.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function() {
-    isElementLoaded("#LoadingContainer").then((selector) => {
-        document.getElementById("LoadingContainer").classList.toggle("hidden")
-    })
+    //isElementLoaded("#LoadingContainer").then((selector) => {
+        //document.getElementById("LoadingContainer").classList.toggle("hidden")
+    //})
 })
